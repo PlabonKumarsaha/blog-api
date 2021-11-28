b20f578e53e4a560b84daf4e3e9714491e1b41ce
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-11-28 23:24:02 +0600

    service class added

---
+++ src/main/java/com/pks/blog/service/PostService.java
@@ -0,0 +1,8 @@
+package com.pks.blog.service;
+
+import com.pks.blog.dto.PostDTO;
+
+public interface PostService {
+
+    public PostDTO createPost(PostDTO postDTO);
+}
---
+++ src/main/java/com/pks/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,36 @@
+package com.pks.blog.service.impl;
+
+import com.pks.blog.dto.PostDTO;
+import com.pks.blog.entity.Post;
+import com.pks.blog.repository.PostRepository;
+import com.pks.blog.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDTO createPost(PostDTO postDTO) {
+        //convert post to entity
+        Post post = new Post();
+        post.setId(postDTO.getId());
+        post.setTitle(postDTO.getTitle());
+        post.setDescription(postDTO.getDescription());
+        post.setContent(postDTO.getContent());
+        Post newPost =postRepository.save(post);
+
+        PostDTO postResponse = new PostDTO();
+        postResponse.setId(newPost.getId());
+        postResponse.setTitle(newPost.getTitle());
+        postResponse.setDescription(newPost.getDescription());
+        postResponse.setContent(newPost.getContent());
+        
+        return postResponse;
+    }
+}
