7484bf8d166045a22989045d81279399853ad877
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-12-04 14:51:09 +0600

    signup using username or email done

--- src/main/java/com/pks/blog/config/SecurityConfig.java
+++ src/main/java/com/pks/blog/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/api/**").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
