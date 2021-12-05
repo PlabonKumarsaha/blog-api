38600a142cbcb8ac82f14889b3099e53787827d1
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-12-05 13:18:43 +0600

    jwt filter

--- src/main/java/com/pks/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/com/pks/blog/security/JwtAuthenticationFilter.java
@@ -1,5 +1,12 @@
 package com.pks.blog.security;
 
+import com.pks.blog.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,11 +18,42 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     //requred dependency inject
-    
+    @Autowired
+    JwtTokenProvider tokenProvider;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
+        //get JWT from HTTP request
+        String token =getJWTfromRequest(request);
+        //validate token
+        if(StringUtils.hasText(token) && tokenProvider.validateToken(token)){
+
+            //get username from token
+            String userName = tokenProvider.getUserNameFromJWT(token);
+            //load user associated with the token
+            UserDetails userDetails =  customUserDetailsService.loadUserByUsername(userName);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new
+                    UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            //set spring security
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    //Bearer access token
+    private String getJWTfromRequest(HttpServletRequest httpServletRequest){
+        String bearerToken = httpServletRequest.getHeader("Authorization");
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7, bearerToken.length());
+        }
+    return null;
     }
 }
